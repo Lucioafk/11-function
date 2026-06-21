ab9b6082e9dac382fab0c7098b5409c2bb966195
Lucioafk <luciogabriel1550@gmail.com>
2026-06-21 16:14:57 -0300

    init

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+function cumprimentarCLiente () {
+    console.log(`Olá, tudo bem? Seja bem-vindo ao restaurante`);
+    console.log(`Hoje temos biscoito de: chocolate, morango e chocolate branco`)
+};
+
+cumprimentarCLiente );
+
+function subtração() {
+
+};
